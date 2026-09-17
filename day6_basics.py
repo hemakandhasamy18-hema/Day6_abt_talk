1ae96e065d34f01df114f645593cc20657d10fb2
hemakandhasamy18-hema <hemakandhasamy18@gmail.com>
2026-09-17 20:13:54 +0530

    Update and rename day5_basics.py to day6_basics.py

---
+++ day6_basics.py
@@ -0,0 +1,18 @@
+n = int(input("Enter N: "))
+
+with open("fizzbuzz_output.txt", "w") as file:
+    for i in range(1, n + 1):
+
+        if i % 3 == 0 and i % 5 == 0:
+            result = "FizzBuzz"
+        elif i % 3 == 0:
+            result = "Fizz"
+        elif i % 5 == 0:
+            result = "Buzz"
+        else:
+            result = str(i)
+
+        print(result)
+        file.write(result + "\n")
+
+print("FizzBuzz output saved to fizzbuzz_output.txt")
